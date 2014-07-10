76841205426f8a14cbf0061d5d158252e2cd6ee0
coffeevanillaicecream <coffeevanillaicecream@gmail.com>
2014-07-10 14:54:49 +0800

    First Push Master Branch

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'poslaravel.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => '',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,8 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/mytest', function()
+{
+    return "Oh yeah, this really works !";
+});
